3a8b135f0879adc12a51a3270321b7b143a031c6
Myka Kugaya <mykakug129@gmail.com>
2020-06-02 23:02:44 -0700

    get and create methods working, update and delete not yet

--- controllers/burgerRoutes.js
+++ controllers/burgerRoutes.js
@@ -36,7 +36,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -48,7 +47,6 @@ router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
   burger.delete(condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
